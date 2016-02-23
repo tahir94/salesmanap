d1ea86226c4c4787f1ca86b21e0063ed3ea94a08
tahir <tahirkhan.on@gmail.com>
2016-02-23 07:00:45 -0800

    map add 2

--- static/myapp/www/js/dashboard.js
+++ static/myapp/www/js/dashboard.js
@@ -8,6 +8,28 @@ app.controller("dashboardController", function ($interval, $location, firebaseRe
 
   $scope.showNotification = function (notification) {
     $scope.notification = notification;
+    console.log(notification);
+
+    angular.extend($scope, {
+     london: {
+     lat: notification.lat,
+     lng: notification.long,
+     zoom: 17
+     },
+     defaults: {
+     zoomAnimation: false,
+     markerZoomAnimation: false,
+     fadeAnimation: false
+     },
+     markers: {
+     london: {
+       lat: notification.lat,
+       lng: notification.long
+     }
+     }
+     });
+
+
     $scope.saveNotification();
     $state.go("dashboard.notifications")
   };
@@ -29,39 +51,6 @@ app.controller("dashboardController", function ($interval, $location, firebaseRe
     })
   };
   $scope.saveNotification();
-
-  /*
-
-   $scope.positions = [{
-   lat: 43.07493,
-   lng: -89.381388
-   }];
-
-   $scope.$on('mapInitialized', function(event, map) {
-   $scope.map = map;
-   });
-
-   $scope.centerOnMe= function(){
-   $scope.positions = [];
-
-
-   $ionicLoading.show({
-   template: 'Loading...'
-   });
-
-
-   navigator.geolocation.getCurrentPosition(function(position) {
-   var pos = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
-   $scope.positions.push({lat: pos.k,lng: pos.B});
-   console.log(pos);
-   $scope.map.setCenter(pos);
-   $ionicLoading.hide();
-   });
-
-   };
-   */
-
-
   $scope.isLoggedIn = true;
   $scope.abc = false;
 
