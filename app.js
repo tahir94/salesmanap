193db68bccb64ff34d95f56731a27c26a56ad8ae
tahir <tahirkhan.on@gmail.com>
2016-02-23 06:24:46 -0800

    map add

--- app.js
+++ app.js
@@ -288,8 +288,8 @@ app.get("/getproduct/:token", function (req, res) {
 
 });
 
- 
- app.set('port', process.env.PORT || 3000);
+
+app.set('port', process.env.PORT || 3000);
 
 var server = app.listen(app.get('port'));
 
--- static/myapp/www/js/login.js
+++ static/myapp/www/js/login.js
@@ -1,50 +1,49 @@
 var app = angular.module("starter");
-app.controller("loginController",function($scope,$http,$state,$rootScope){
+app.controller("loginController", function ($scope, $http, $state, $rootScope) {
 
 
-    $rootScope.isLogin = false;
-   // $scope.required = true;
+  $rootScope.isLogin = false;
+  // $scope.required = true;
 
 
-  $scope.login = function(loginUser){
-      //console.log(loginUser);
+  $scope.login = function (loginUser) {
+    //console.log(loginUser);
 
-      $http.post("/login",loginUser)
-       .then(function(success){
+    $http.post("/login", loginUser)
+      .then(function (success) {
 
-           if(success.data.success == false){
-              console.log(success.data.success);
-               console.log(success.data.message);
-               console.log(success)
-           }
-           else{
-           //console.log("success" ,success);
-           //console.log(success.data.message) ;
-           localStorage.setItem("firebaseToken",success.data.data.firebaseToken);
-            $rootScope.userData = success.data.data.firebaseToken;
+        if (success.data.success == false) {
+          console.log(success.data.success);
+          console.log(success.data.message);
+          console.log(success)
+        }
+        else {
+          //console.log("success" ,success);
+          //console.log(success.data.message) ;
+          localStorage.setItem("firebaseToken", success.data.data.firebaseToken);
+          $rootScope.userData = success.data.data.firebaseToken;
 
-            // if($rootScope.getData == null){
+          // if($rootScope.getData == null){
 
-            //     $state.go("dashboard.createCompany")
-            // }
-            // else{
-            //     $state.go("dashboard.showCompany")
-            // }
-
-
-
-     //   $state.go("dashboard")
-     $state.go("dashboard");
+          //     $state.go("dashboard.createCompany")
+          // }
+          // else{
+          //     $state.go("dashboard.showCompany")
+          // }
 
 
-       }
-       }),
-       function(err){
-           console.log('err',err);
-       }
+          //   $state.go("dashboard")
+          $state.go("dashboard");
+
+
+        }
+      }),
+      function (err) {
+        console.log('err', err);
+      }
 
 
   }
-})
+});
 
 
